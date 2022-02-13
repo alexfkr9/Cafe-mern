859351a297456061002155ad16d94e7df1bd9b28
Alex <alexfkr9@gmail.com>
2022-02-13 11:56:26 +0200

    4

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 
-const db = "mongodb+srv://san:master9@cluster0.uksn7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const db = "mongodb+srv://xxxxxxxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 async function start() {
     try {        
